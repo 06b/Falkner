e01f2f49c20342963e1547982bdf07380d784148
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-10-19 23:49:44 -0400

    Starting point
    Still have a long way to go

---
+++ src/Falkner/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Falkner.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
